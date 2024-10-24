b5ddb7d18a1737700d948e3727d993a6f951da3d
thanapat.k <thanapat.ki@kkumail.com>
2024-10-24 17:20:05 +0700

    add ByteArkPlayerContainer and config nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,48 @@
-import Image from "next/image";
+import { ByteArkPlayerContainer } from "byteark-player-react";
+import type {
+  ByteArkPlayerContainerProps,
+  ByteArkPlayer,
+} from "byteark-player-react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const VIDEO_SOURCE_URL =
+  "https://tcctechuprwieg.stream-playlist.byteark.com/streams/UPvCYWWDPd93/playlist.m3u8";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+export default function VideoTestPage() {
+  const options: ByteArkPlayerContainerProps = {
+    fluid: true,
+    autoplay: "any",
+    muted: true,
+    aspectRatio: "16:9",
+    // hide picture in picture button
+    disablePictureInPicture: true,
+    // workaround for play VR/360° video on Safari macOS/iOS
+    // set crossorigin for Safari macOS/iOS
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    crossorigin: "anonymous",
+    controlBar: {
+      pictureInPictureToggle: false,
+    },
+    plugins: {
+      vr: {
+        projection: "360",
+        motionControls: true,
+        sphereDetail: 128,
+        debug: true,
+      },
+      bytearkLighthouse: {
+        projectId: "tcctech-1h9rax6noal",
+      },
+    },
+    onReady(player) {
+      // workaround for play VR/360° video on Safari macOS/iOS
+      // set source after player create
+      player.src({
+        src: VIDEO_SOURCE_URL,
+        type: "application/x-mpegURL",
+      });
+    },
+  };
+
+  return <ByteArkPlayerContainer {...options} />;
 }
